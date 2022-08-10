60ae2e2e0553791525a928a1312b45ff32f63f17
Demid Yermak <d.yermak@artjoker.net>
2022-08-11 00:56:16 +0300

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+$('.js__pagescroll').fullpage({
+  //options here
+  autoScrolling: true,
+  sectionSelector: '.js__section',
+  responsiveWidth: false,
+  scrollBar: true,
+  normalScrollElements: '.normal-scrolling',
+  fitToSection: false,
+  verticalCentered: false // fixedElements: '.header',
+
+});
